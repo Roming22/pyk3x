96a0b8f0b8947e3ef1597a461237ac556e271dce
Roming22 <roming22@gmail.com>
2021-02-13 23:43:57 +0000

    Automate versioning of releases (production, development and wip)

--- src/k3x/__init__.py
+++ src/k3x/__init__.py
@@ -1,5 +1,16 @@
 """Package registration"""
-
-__version__ = "0.0.1"
+from os import path
 
 from .module1 import *
+
+try:
+    with open(path.join(path.dirname(__file__), "version.txt"), "r") as file:
+        __version__ = file.readline().strip()
+except:  # pylint: disable=bare-except
+    __version__ = "0.0.dev0"
+
+try:
+    with open(path.join(path.dirname(__file__), "commit.txt"), "r") as file:
+        __commit__ = file.readline().strip()
+except:  # pylint: disable=bare-except
+    __commit__ = "unknown"
